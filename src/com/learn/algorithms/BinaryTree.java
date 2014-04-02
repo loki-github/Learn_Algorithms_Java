1e806868be69d8359604f5f63c750d19c0a2eb26
lokesh <lokesh.ck@gmail.com>
2014-04-02 00:09:09 -0700

    added Binary Tree class

---
+++ src/com/learn/algorithms/BinaryTree.java
@@ -0,0 +1,19 @@
+package com.learn.algorithms;
+
+class BTNode{
+	int info;
+	Node left;
+	Node right;
+}
+
+
+
+public class BinaryTree {
+
+	static BTNode root=null;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
